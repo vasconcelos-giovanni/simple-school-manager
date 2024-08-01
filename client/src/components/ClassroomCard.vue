8cb242dbbfe4419de5e453128eb217c3156bb746
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2024-08-01 14:29:32 -0300

    feat: make Classroom card

---
+++ client/src/components/ClassroomCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="card"
+    style="width: 18rem;"
+  >
+    <div class="card-body">
+      <h5 class="card-title fw-bold">
+        Sociologia II
+      </h5>
+      <ul class="no-bulleted-list">
+        <li class="list-group-item">
+          <span class="fw-bold">Ano:</span>
+          5
+        </li>
+        <li class="list-group-item">
+          <span class="fw-bold">Período:</span>
+          Vespertino
+        </li>
+      </ul>
+      <div class="d-flex justify-content-end gap-1 mt-2">
+        <a
+          href="#"
+          class="btn btn-outline-primary btn-sm"
+        >Detalhar</a>
+        <a
+          href="#"
+          class="btn btn-outline-warning btn-sm"
+        >Editar</a>
+        <a
+          href="#"
+          class="btn btn-outline-danger btn-sm"
+        >Excluir</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.no-bulleted-list {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+</style>
